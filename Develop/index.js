e1a914619f920b01ee06e0a37e2ea21951ffb050
Leandro Parrado <lparrado321@gmail.com>
2021-01-02 20:27:15 -0500

    Modified index.js. Added timetracker() function, wrote comments. Incomplete.

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings("description").val();
+        var time = $(this).parent().attr("id");
+
+        // Saves user input in local storage.
+        localStorage.setItem(time, text);
+    })
+
+    function timeTracker() {
+        // Gets current number of hours.
+        var timeNow = moment().hour();
+
+        //loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        })
+    }
 
 })
 
+//Current day and year display at top of page.
 var todayDate = moment().format('ddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
